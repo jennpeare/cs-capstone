6b3596cc6e3a9d94f01b89b2a9eda74a4fe87d57
Jenny Shi <jennpeare@gmail.com>
2013-10-22 19:56:21 -0400

    added Building class

---
+++ src/schedule/Building.java
@@ -0,0 +1,18 @@
+package schedule;
+
+public class Building {
+	
+	String building_id;
+	String building_number;
+	String campus_code;
+	String campus_name;
+	String cid;
+	String description;
+	String title;
+	String[] offices;
+	
+	public Building() {
+		
+	}
+	
+}
