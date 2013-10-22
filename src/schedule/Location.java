d05af6050643940f43cca5a8cfd9f50ceaba3117
Billy Lynch <wlynch92@gmail.com>
2013-10-22 19:46:38 -0400

    added Location class

---
+++ src/schedule/Location.java
@@ -0,0 +1,16 @@
+package schedule;
+
+public class Location {
+	String additional;
+	String city;
+	String country;
+	String country_abbr;
+	String latitude;
+	String longitude;
+	String name;
+	String postal_code;
+	String state;
+	String state_abbr;
+	String street;
+
+}
