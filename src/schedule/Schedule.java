bcf2273dd3fdb41c4b26ea0e43150e789ef6e2eb
Billy Lynch <wlynch92@gmail.com>
2013-10-22 23:48:47 -0400

    Added serializedname annotations. Added treemap functions.

--- src/schedule/Schedule.java
+++ src/schedule/Schedule.java
@@ -4,11 +4,21 @@ import java.io.IOException;
 import java.io.File;
 import java.lang.reflect.Type;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+
+/**
+ * @author Billy Lynch
+ *
+ */
 public class Schedule {
+	
+	/**
+	 * @param args
+	 */
 	public static void main(String args[]) {
 		Gson gson = new Gson();
 		try {
@@ -16,16 +26,27 @@ public class Schedule {
 			Type collectionType = new TypeToken<Building[]>(){}.getType();
 			Building[] buildings = gson.fromJson(json, collectionType);
 			
+			TreeMap<Integer, Classroom> classrooms = new TreeMap<Integer, Classroom>();
+			buildTree(classrooms);
+			
 			json = readFile(args[1]);
 			collectionType = new TypeToken<Course[]>(){}.getType();
 			Course[] courses = gson.fromJson(json, collectionType);
-			//System.out.println(courses[7].sections[0].instructors[0].name);
-			System.out.println(courses[7]);
+			for (Course c: courses) {
+				for (Section s: c.sections) {
+					System.out.println(c.title + " " + classrooms.floorKey(s.stopPoint));
+				}
+			}
 		} catch (IOException e) {
 			System.out.println("ERROR");
 		}
 	}
 
+	/**
+	 * @param pathname
+	 * @return
+	 * @throws IOException
+	 */
 	private static String readFile(String pathname) throws IOException {
 		File file = new File(pathname);
 		StringBuilder fileContents = new StringBuilder((int)file.length());
@@ -42,4 +63,5 @@ public class Schedule {
 		}
 	}
 
+	
 }
